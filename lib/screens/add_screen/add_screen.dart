1ace36d3dd09d4643f30fd482b6243d74e828881
Aleksey Lomov <zibloidix@gmail.com>
2020-05-18 07:42:54 +1100

    Fix addScreen outside

--- lib/screens/add_screen/add_screen.dart
+++ lib/screens/add_screen/add_screen.dart
@@ -13,81 +13,84 @@ class AddScreenState extends State<AddScreen> {
         builder: (context, constraints) => Container(
           width: constraints.maxWidth,
           height: constraints.maxHeight,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: constraints.maxHeight / 2,
-                color: Colors.deepPurpleAccent[700],
-              ),
-              Container(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    // SizedBox(height: 80,),
-                    // Container(
-                    //   width: 250,
-                    //   child: Center(child: Image.asset('assets/images/add.png')),
-                    // ),
-                    Container(
-                      width: 350,
-                      height: 550,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: [BoxShadow(
-                          offset: Offset(0.0, 1.0),
-                          blurRadius: 4.0,
-                          color: Colors.black26
-                        ),]
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(height: 120,),
-                          Container(
-                            width: 120,
-                            height: 120,
-                            child: Center(
-                              child: Image.asset(
-                                'assets/images/add.png',
-                                width: 110,
-                                height: 110,
+          child: SingleChildScrollView(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: 300,
+                  color: Colors.deepPurpleAccent[700],
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      // SizedBox(height: 80,),
+                      // Container(
+                      //   width: 250,
+                      //   child: Center(child: Image.asset('assets/images/add.png')),
+                      // ),
+                      SizedBox(height: 120,),
+                      Container(
+                        width: 300,
+                        height: 490,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [BoxShadow(
+                            offset: Offset(0.0, 1.0),
+                            blurRadius: 4.0,
+                            color: Colors.black26
+                          ),]
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 90,),
+                            Container(
+                              width: 120,
+                              height: 120,
+                              child: Center(
+                                child: Image.asset(
+                                  'assets/images/add.png',
+                                  width: 110,
+                                  height: 110,
+                                ),
                               ),
                             ),
-                          ),
-                          SizedBox(height: 40,),
-                          Container(
-                            width: 240,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                prefixIcon: Icon(Icons.link),
-                                labelText: 'Ссылка на структуру',
-                              )
+                            SizedBox(height: 40,),
+                            Container(
+                              width: 240,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.link),
+                                  labelText: 'Ссылка на структуру',
+                                )
+                              ),
                             ),
-                          ),
-                          Container(
-                            width: 240,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                prefixIcon: Icon(Icons.link),
-                                labelText: 'Ссылка на набор'
-                              )
+                            Container(
+                              width: 240,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.link),
+                                  labelText: 'Ссылка на набор'
+                                )
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 40,),
-                          RaisedButton(
-                            color: Colors.indigoAccent[400],
-                            child: Text('Загрузить', style: TextStyle(color: Colors.white),),
-                            onPressed: (){},
-                          )
-                        ],
+                            SizedBox(height: 40,),
+                            RaisedButton(
+                              color: Colors.indigoAccent[400],
+                              child: Text('Загрузить', style: TextStyle(color: Colors.white),),
+                              onPressed: (){},
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
